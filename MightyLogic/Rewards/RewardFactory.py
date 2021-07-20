97c0471dd67395e0d0c502ffe410381a2705c579
HausReport <tj_willis@hotmail.com>
2021-07-20 00:35:40 -0400

    tweaking

--- MightyLogic/Rewards/RewardFactory.py
+++ MightyLogic/Rewards/RewardFactory.py
@@ -13,22 +13,22 @@ def rare_chest(verbose=False):
         souls = randrange(80, 220)
         if verbose:
             print(f"{souls} rare souls")
-        return Reward(name="Rare Chest", rare=souls)
+        return Reward(myName="Rare Chest", rare=souls)
     elif branch < 77:
         souls = 20
         if verbose:
             print(f"{souls} epic souls")
-        return Reward(name="Rare Chest", epic=souls)
+        return Reward(myName="Rare Chest", epic=souls)
     elif branch < 97:
         souls = randrange(175, 300)
         if verbose:
             print(f"{souls} sparks")
-        return Reward(name="Rare Chest", spark=souls)
+        return Reward(myName="Rare Chest", spark=souls)
     else:
         souls = randrange(50, 100)
         if verbose:
             print(f"{souls} gems")
-        return Reward(name="Rare Chest", gem=souls)
+        return Reward(myName="Rare Chest", gem=souls)
 
 
 def epic_chest(verbose=False):
@@ -37,22 +37,22 @@ def epic_chest(verbose=False):
         souls = randrange(80, 500)
         if verbose:
             print(f"{souls} epic souls")
-        return Reward(name="Epic Chest", epic=souls)
+        return Reward(myName="Epic Chest", epic=souls)
     elif branch < 77:
         souls = 20
         if verbose:
             print(f"{souls} legendary souls")
-        return Reward(name="Epic Chest", legendary=souls)
+        return Reward(myName="Epic Chest", legendary=souls)
     elif branch < 97:
         souls = randrange(750, 2000)
         if verbose:
             print(f"{souls} sparks")
-        return Reward(name="Epic Chest", spark=souls)
+        return Reward(myName="Epic Chest", spark=souls)
     else:
         souls = randrange(150, 300)
         if verbose:
             print(f"{souls} gems")
-        return Reward(name="Epic Chest", gem=souls)
+        return Reward(myName="Epic Chest", gem=souls)
 
 
 def legendary_chest(verbose=False):
@@ -61,14 +61,14 @@ def legendary_chest(verbose=False):
         souls = randrange(40, 160)
         if verbose:
             print(f"{souls} legendary souls")
-        return Reward(name="Legendary Chest", legendary=souls)
+        return Reward(myName="Legendary Chest", legendary=souls)
     elif branch < 97:
         souls = randrange(3500, 10000)
         if verbose:
             print(f"{souls} sparks")
-        return Reward(name="Legendary Chest", spark=souls)
+        return Reward(myName="Legendary Chest", spark=souls)
     else:
         souls = 1000
         if verbose:
             print(f"{souls} gems")
-        return Reward(name="Legendary Chest", gem=souls)
+        return Reward(myName="Legendary Chest", gem=souls)
