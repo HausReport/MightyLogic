d2b5cffb91d5276ffcdc28de4722652279f6d899
HausReport <tj_willis@hotmail.com>
2021-07-19 22:45:35 -0400

    starting packaging stuff

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='MightyLogic',
     version='0.1',
-    packages=['org.MightyLogic', 'org.MightyLogic.TurfWar', 'org.MightyLogic.TurfWar.Tiles'],
+    packages=['MightyLogic', 'MightyLogic.TurfWar', 'MightyLogic.TurfWar.Tiles'],
     package_dir={'': 'org'},
     url='',
     license='',
