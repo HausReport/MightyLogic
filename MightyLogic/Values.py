1f7a0478b28081e6eaf97ada9de497d8f507ff5b
HausReport <tj_willis@hotmail.com>
2021-07-20 00:38:09 -0400

    tweaking

--- MightyLogic/Values.py
+++ MightyLogic/Values.py
@@ -2,7 +2,7 @@ class Values():
     COMMON_SOUL = 0.001 * 1.0
     RARE_SOUL = 0.01 * 1.0
     SOUL_DUST = 0.05 * 1.0
-    EPIC_SOUL: 0.1 * 1.0
+    EPIC_SOUL =  0.1 * 1.0
     GOLD = 0.6 * 1.0
     KILO_SOUL = 1 * 1.0
     LEGENDARY_SOUL = 2 * 1.0
