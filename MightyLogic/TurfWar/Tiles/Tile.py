086207b48662dc3d53298152542dffcf4d07b1ab
HausReport <tj_willis@hotmail.com>
2021-07-20 02:25:30 -0400

    tweaking

--- MightyLogic/TurfWar/Tiles/Tile.py
+++ MightyLogic/TurfWar/Tiles/Tile.py
@@ -3,13 +3,19 @@ from MightyLogic.Rewards.Reward import Reward
 
 class Tile:
 
-    def __init__(self, name, row, column, value=0, common=0, rare=0, soul_dust=0, epic=0, gold=0, legendary=0,
+    def __init__(self, name, row=0, column=0, value=0, common=0, rare=0, soul_dust=0, epic=0, gold=0, legendary=0,
                  contrib=0, influence=0, spark=0, gem=0):
         self.name = name
         self.row = row
         self.column = column
         self.reward = Reward(value, common, rare, soul_dust, epic, gold, legendary, contrib, influence, spark, gem)
-        print("world")
+        # print("world")
+
+    def setRow(self,row):
+        self.row = row
+
+    def setColumn(self, column):
+        self.column = column
 
     def share(self, percent):
         self.reward.share(percent)
