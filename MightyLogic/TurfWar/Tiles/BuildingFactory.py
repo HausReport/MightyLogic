9de10e97fd477cb9858857871bdea80b828b0f68
HausReport <tj_willis@hotmail.com>
2021-07-20 16:42:30 -0400

    tweaking

--- MightyLogic/TurfWar/Tiles/BuildingFactory.py
+++ MightyLogic/TurfWar/Tiles/BuildingFactory.py
@@ -2,32 +2,32 @@ from MightyLogic.TurfWar.Tiles.Tile import Tile
 
 
 def altar(epic, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Altar", row=row, column=column, epic=epic, influence=influence, contrib=contrib)
+    return Tile(name="Altar", row=row, column=column, epic=epic, influence=influence, contrib=contrib, building=True)
 
 
 # ===============================================================================================================
 def factory(soul_dust, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Factory", soul_dust=soul_dust, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Factory", soul_dust=soul_dust, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 def gold_mine(gold, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Gold Mine", gold=gold, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Gold Mine", gold=gold, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 def hells_gate(epic, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Hell's Gate", epic=epic, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Hell's Gate", epic=epic, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 def lighthouse(rare, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Lighthouse", rare=rare, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Lighthouse", rare=rare, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 def tree_of_life(rare, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Tree of Life", rare=rare, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Tree of Life", rare=rare, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 def witch_hut(rare, influence, contrib, row="?", column="?", building=True):
-    return Tile(name="Witch Hut", rare=rare, influence=influence, contrib=contrib, row=row, column=column)
+    return Tile(name="Witch Hut", rare=rare, influence=influence, contrib=contrib, row=row, column=column, building=True)
 
 
 #
