b5bf89293134a4730d81e0b64e4b3705f6353a17
HausReport <tj_willis@hotmail.com>
2021-09-11 09:42:44 -0400

    bug fix

--- MightyLogic/TurfWar/Tiles/TileList.py
+++ MightyLogic/TurfWar/Tiles/TileList.py
@@ -64,7 +64,7 @@ class TileList:
         rew = Reward(myName="Combo")
         for tile in self.tiles:
             #print(tile.reward)
-            rew = rew.combine(tile.gems)
+            rew = rew.combine(tile.reward)
         return rew
 
     def getTileLine(self):
