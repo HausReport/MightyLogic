c6ee01d266a406d6996245338d1e7c1d11cd4eae
HausReport <tj_willis@hotmail.com>
2021-09-11 09:19:36 -0400

    add 2nd guild

---
+++ MightyLogic/Player/Nightmare.py
@@ -0,0 +1,51 @@
+from MightyLogic.Player.Player import Player
+from MightyLogic.Player.PlayerGroup import PlayerGroup
+
+def getNightTerrorPlayerGroup() -> PlayerGroup:
+        players = PlayerGroup()
+        players.append(Player("Caramel2", 58000))         # x
+        players.append(Player("Bobo", 104530))
+        players.append(Player("1337", 73611))         # x
+        players.append(Player("Tinkrer", 27555))         # x
+        players.append(Player("SoulD3aD", 82274))
+        players.append(Player("Felipe", 77019))         # x
+        players.append(Player("Ronnie", 66445))         # x
+        players.append(Player("Antarenix", 52259))         # x
+
+        players.append(Player("StoneHamme", 55913))         # x
+        players.append(Player("itax", 52270))         # x
+        players.append(Player("Ferminotok", 41003))         # x
+        players.append(Player("Adam", 96195))         # x
+        players.append(Player("Adolph", 90622))         # x
+        players.append(Player("Gold", 53736))         # x
+        players.append(Player("Zelektro", 57477))         # x
+
+        players.append(Player("Stormrager", 42199))         # x
+        players.append(Player("SirBrychee", 41725))         # x
+        players.append(Player("Aliska", 31633))         # x
+        players.append(Player("MehHair", 36229))         # x
+        players.append(Player("AliGryphon", 88840))         # x
+        players.append(Player("Vrael", 45516))         # x
+        players.append(Player("freya", 111277))         # x
+        players.append(Player("Cerryl", 74153))         # x
+
+        players.append(Player("NotAdam", 76986))         # x
+        players.append(Player("Sebastian", 46497))         # x
+        players.append(Player("UrPalGrant", 104275))         # x
+        players.append(Player("Anarchy", 86407))         # x
+        players.append(Player("Wyv22", 77966))         # x
+        players.append(Player("Tron", 71825))         # x
+        players.append(Player("EarEater", 68973))         # x
+        players.append(Player("Zombi", 65766))         # x
+
+        players.append(Player("Milkopilko", 61516))         # x
+        players.append(Player("Donny", 55085))         # x
+        players.append(Player("MightyPups", 51839))         # x
+        players.append(Player("MikeLouie", 47425))         # x
+        players.append(Player("incertus", 38228))         # x
+        players.append(Player("Alakay", 37714))         # x
+        players.append(Player("mmm_gravy", 36504))         # x
+        players.append(Player("AXIL", 36212))         # x
+        players.append(Player("Innersab", 33793))         # x
+
+        return players
