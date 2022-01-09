83523e26c02c855935948ffeaee8317548eb03fd
HausReport <tj_willis@hotmail.com>
2022-01-09 01:34:52 -0500

    packaging

--- MightyLogic/HighGrowth/Erlaed/Rarity.py
+++ MightyLogic/HighGrowth/Erlaed/Rarity.py
@@ -134,3 +134,78 @@ class Rarity(ABC):
     def get_reborn_1_point(self, df, rb=1):
         pass
 
+    def get_moves(self, level, reborn, avail_souls, total_souls=-1, avail_gold=-1):
+        (curMight, curTroops) = self.lookup(reborn, level)
+        moves = fancy_level(level, reborn, avail_souls, total_souls, avail_gold)
+        moves["Cur Level"] = level
+        moves["Cur Reborn"] = reborn
+        moves["Troop Gain"] = moves["Troops"] - curTroops
+        moves = moves[moves['Troop Gain'] > 0]
+        moves = moves[moves['Level'] > 10]
+        moves['LevelUps'] = 0
+
+        if len(moves) > 0:
+            moves['LevelUps'] = moves.apply(
+                lambda x: self.level_dist(x["Cur Reborn"], x["Cur Level"], x["Reborn"], x["Level"]),
+                axis=1)  # (reborn, level, rb1, l1)
+        # rb1 = moves['Reborn'].values[0]
+        # l1 = moves['Level'].values[0]
+
+        #
+        #  YO!  Pretty sure lamba chokes when result has 0 rows
+        #
+
+        # moves['LevelUps'] = moves.apply(lambda x: level_dist(reborn, level, x['Reborn'], x['Level']), axis=1) #(reborn, level, rb1, l1)
+
+        moves["Score"] = 10000 * (moves["Troop Gain"] / moves["Cum Gold"])
+        return moves
+
+    def get_moves_by_name(self, name, avail_gold=-1):
+        if ((all['Name'] == name)).any():
+            loc = all.loc[all['Name'] == name]
+            level = loc.Level.values[0]
+            reborn = loc.Reborns.values[0]
+            avail_souls = loc["Available Souls"].values[0]
+            total_souls = loc["Total Souls"].values[0]
+            return self.get_moves(level, reborn, avail_souls, total_souls, avail_gold)
+        else:
+            return None
+
+    def fancy_level(self, level, reborn, avail_souls, total_souls=-1, avail_gold=-1):
+        tab = self.straight_level(level, reborn, avail_souls, avail_gold)
+
+        #
+        # Have to add extra suppoort for over-levelled heroes
+        # i.e. R 0 L 10
+        # add a row for R 0 L 6
+        #   add parm total_souls
+        #   spent_souls = total_souls-avail_souls
+        #   figure required_souls
+        #
+        #   set cum_souls to show "rebate" spent_souls-required_souls
+        #   set cum_gold to 0
+        #
+
+        #
+        # Table of possible levelups
+        #
+        for rb in range(reborn, 5):
+            if self.has_reborn_1(tab, rb=rb):
+
+                (cs, cg) = self.get_reborn_1_point(tab, rb=rb)
+                tmp = self.get_reborn_table(rb).copy(deep=True)
+                tmp.loc[0, 'Gold'] = cg
+                tmp.loc[0, 'Souls'] = cs
+
+                tmp['Cum Souls'] = tmp.Souls.cumsum()
+                tmp['Cum Gold'] = tmp.Gold.cumsum()
+                tmp = tmp[tmp['Cum Souls'] <= avail_souls]
+                if avail_gold > 0:
+                    tmp = tmp[tmp['Cum Gold'] <= avail_gold]
+                tab = tab.append(tmp)
+            elif reborn == rb:
+                if level >= self.reborn_level(rb + 1):
+                    tmp = self.get_tmp_table(total_souls, avail_souls, avail_gold, rb)
+                    tab = tab.append(tmp)
+
+        return tab
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MightyLogic',
-    version='0.36',
+    version='0.37',
     packages=[
         'MightyLogic',
         'MightyLogic.Heroes',
