abb6e0bb1faaeb6ff24e0e44e8abdc37d28e3087
HausReport <tj_willis@hotmail.com>
2022-01-10 01:47:55 -0500

    rare support

---
+++ MightyLogic/HighGrowth/Erlaed/Rare.py
@@ -0,0 +1,85 @@
+import pandas
+import pandas as pd
+
+from MightyLogic.HighGrowth.Erlaed.PurpleGold import PurpleGold
+
+class Epic(PurpleGold):
+
+    def get_r0_tab(self) -> pandas.DataFrame:
+        r0_tab_epic = pd.DataFrame([[1, 120, 25, 0, 0],
+                                    [2, 135, 28, 25, 100],
+                                    [3, 150, 31, 50, 300],
+                                    [4, 170, 34, 75, 600],
+                                    [5, 185, 37, 100, 900],
+                                    [6, 200, 40, 130, 1400],
+                                    [7, 230, 46, 160, 2000],
+                                    [8, 260, 52, 190, 3500],
+                                    [9, 290, 58, 220, 4000],
+                                    [10, 320, 64, 250, 5000],
+                                    [11, 350, 70, 330, 6000],
+                                    [12, 400, 82, 420, 8000],
+                                    [13, 450, 94, 500, 10000],
+                                    [14, 500, 106, 600, 12000],
+                                    [15, 550, 118, 750, 14000],
+                                    [16, 600, 130, 950, 16000],
+                                    [17, 680, 156, 1400, 19000],
+                                    [18, 760, 182, 1900, 22000],
+                                    [19, 840, 208, 2400, 25000],
+                                    [20, 920, 234, 3000, 28000],
+                                    [21, 1000, 260, 3700, 32000],
+                                    [22, 1140, 288, 4500, 36000],
+                                    [23, 1280, 316, 5500, 40000],
+                                    [24, 1420, 344, 7000, 45000],
+                                    [25, 1560, 372, 9000, 50000],
+                                    [26, 1700, 400, 12000, 55000],
+                                    [27, 1812, 426, 12150, 57000],
+                                    [28, 1924, 452, 12350, 60000],
+                                    [29, 2036, 478, 12550, 63000],
+                                    [30, 2148, 504, 12800, 66000],
+                                    [31, 2260, 530, 13100, 70000]],
+                                   columns=['Level', 'Might', 'Troops', 'Souls', 'Gold'])
+
+        r0_tab_epic['Gold'] = (r0_tab_epic['Gold'] * self.gold_discount)
+        return r0_tab_epic
+
+    def getMightBonus(self, reborn):
+        if reborn == 0:
+            return 0
+        elif reborn == 1:
+            return 150
+        elif reborn == 2:
+            return 200 + 150
+        elif reborn == 3:
+            return 350 + 200 + 150
+        elif reborn == 4:
+            return 600 + 350 + 200 + 150
+        elif reborn == 5:
+            return 1000+ 600 + 350 + 200 + 150
+
+    def getTroopBonus(self, reborn):
+        if reborn == 0:
+            return 0
+        elif reborn == 1:
+            return 60
+        elif reborn == 2:
+            return 145 + 60
+        elif reborn == 3:
+            return 360 + 145 + 60
+        elif reborn == 4:
+            return 750 + 360 + 145 + 60
+        elif reborn == 5:
+            return 1350 + 750 + 360 + 145 + 60
+
+    def sn(self, rb=0):
+        if rb == 0:
+            return 0
+        elif rb == 1:
+            return 230
+        elif rb == 2:
+            return 230 + 950
+        elif rb == 3:
+            return 230 + 950 + 3070
+        elif rb == 4:
+            return 230 + 950 + 3070 + 10520
+        elif rb == 5:
+            return 230 + 950 + 3070 + 10520 + 36820
--- MightyLogic/HighGrowth/Erlaed/WhiteBlue.py
+++ MightyLogic/HighGrowth/Erlaed/WhiteBlue.py
@@ -21,43 +21,47 @@ class WhiteBlue(Rarity):
                 return 999
 
     def to_ordinal(self, reborn, level):
-        ret = 0
         if reborn == 0:
-            ret = level - 1
-            if ret > 11:
-                ret = 11
+            if level > 11:
+                return self.to_ordinal(0, 11)
+            else:
+                return level - 1
         elif reborn == 1:
-            ret = 11 + level - 2
-            if ret > 16 + 11:
-                ret = 16 + 11
+            if level > 16:
+                return self.to_ordinal(1, 16)
+            else:
+                return level - 1 + self.to_ordinal(0, 11)
         elif reborn == 2:
-            ret = 11 + 16 + level - 3
-            if ret > 11 + 16 + 21:
-                ret = 6 + 11 + 21
+            if level > 21:
+                return self.to_ordinal(2, 21)
+            else:
+                return level - 1 + self.to_ordinal(1, 11)
         elif reborn == 3:
-            ret = 11 + 16 + 21 + level - 4
-            if ret > 11 + 16 + 21 + 26:
-                ret = 11 + 16 + 21 + 26
+            if level > 26:
+                return self.to_ordinal(3, 26)
+            else:
+                return level - 1 + self.to_ordinal(2, 21)
         elif reborn == 4:
-            ret = 6 + 11 + 16 + 21 + level - 5
-            if ret > 11 + 16 + 21 + 26 + 31:
-                ret = 11 + 16 + 21 + 2 + 31
+            if level > 26:
+                return self.to_ordinal(4, 26)
+            else:
+                return level - 1 + self.to_ordinal(3, 26)
         elif reborn == 5:
-            ret = 999
-        return ret
+            if level > 31:
+                return self.to_ordinal(5, 31)
+            else:
+                return level - 1 + self.to_ordinal(4, 26)
 
     def to_ordinal2(self, reborn, level):
-        ret = 0
         if reborn == 0:
-            ret = level - 1
+            return level - 1
         elif reborn == 1:
-            ret = 11 + level - 2
+            return level - 1 + self.to_ordinal(0, 11)
         elif reborn == 2:
-            ret = 11 + 16 + level - 3
+            return level - 1 + self.to_ordinal(1, 16)
         elif reborn == 3:
-            ret = 11 + 16 + 21 + level - 4
+            return level - 1 + self.to_ordinal(2, 21)
         elif reborn == 4:
-            ret = 11 + 16 + 21 + 26 + level - 5
+            return level - 1 + self.to_ordinal(3, 26)
         elif reborn == 5:
-            ret = 999
-        return ret
+            return level - 1 + self.to_ordinal(4, 31)
