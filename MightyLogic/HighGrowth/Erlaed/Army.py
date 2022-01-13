808aebcd63b600165f1d9e0d1d26b9c0e83d23d4
HausReport <tj_willis@hotmail.com>
2022-01-13 00:36:02 -0500

    debugging

--- MightyLogic/HighGrowth/Erlaed/Army.py
+++ MightyLogic/HighGrowth/Erlaed/Army.py
@@ -4,43 +4,45 @@ import plotly.express as px
 
 class Army:
 
+    data_frame: pd.DataFrame
+
     def __init__(self):
         self.data_frame = None
 
     def fromFile(self, file):
         self.data_frame = pd.read_csv(file)
 
-    def fromDataframe(self, frame):
+    def fromDataframe(self, frame: pd.DataFrame):
         self.data_frame = frame
 
-    def getArmy(self):
+    def getArmy(self) -> pd.DataFrame:
         return self.data_frame
 
-    def getLegendaries(self):
+    def getLegendaries(self) -> pd.DataFrame:
         return self.data_frame[self.data_frame['Rarity'] == 'Legendary']
 
-    def getEpics(self):
+    def getEpics(self) -> pd.DataFrame:
         return self.data_frame[self.data_frame['Rarity'] == 'Epic']
 
-    def getRares(self):
+    def getRares(self) -> pd.DataFrame:
         return self.data_frame[self.data_frame['Rarity'] == 'Rare']
 
-    def getCommons(self):
+    def getCommons(self) -> pd.DataFrame:
         return self.data_frame[self.data_frame['Rarity'] == 'Common']
 
-    def getGutterLegendaries(self):
+    def getGutterLegendaries(self) -> pd.DataFrame:
         legos = self.getLegendaries()
         return legos[(legos['Level'] > 1) & (legos['Level'] < 11)]
 
-    def getGutterEpics(self):
+    def getGutterEpics(self) -> pd.DataFrame:
         legos = self.getEpics()
         return legos[(legos['Level'] > 1) & (legos['Level'] < 11)]
 
-    def getGutterRares(self):
+    def getGutterRares(self) -> pd.DataFrame:
         legos = self.getRares()
         return legos[(legos['Level'] > 1) & (legos['Level'] < 11)]
 
-    def getLevelOneLegendaries(self):
+    def getLevelOneLegendaries(self) -> pd.DataFrame:
         legos = self.getLegendaries()
         return legos[(legos['Level'] == 1)]
 
