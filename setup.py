a5f133368a2296229b500c62c7757a6073095850
HausReport <tj_willis@hotmail.com>
2022-01-27 01:17:10 -0500

    kick setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='MightyLogic',
-    version='0.499993',
+    version='0.499994',
     include_package_data=True,
     packages=[
         'MightyLogic',
