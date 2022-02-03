593b225cd0dea8ef145f1a826a0ec6968b05fbc6
dacoleboy <908563+dacoleboy@users.noreply.github.com>
2022-02-03 11:23:07 -0800

    Update discount values, HG config for Bobo/Seph

--- HighGrowthRunner.py
+++ HighGrowthRunner.py
@@ -39,7 +39,123 @@ def pretty_format(thing: Any, indent: int = 0):
     return formatted
 
 
-file = Path("tests/HighGrowth/2022-01-09-1521_Bobo_squad_export.txt")
+# ======================================================================================================================
+# Settings
+# ----------------------------------------------------------------------------------------------------------------------
+
+def bobo_settings():
+    bobo_squad = {  # level/reborn these as much as possible | exclude their evolutions
+        # melee
+        "charon",
+        "groot",
+        "shaa",
+        "mosura",
+
+        # ranged
+        "eostre",
+        "grace",
+        "strik",
+        "villano",  # 1/1
+    }
+    bobo_to_farm = {  # level these heroes but never reborn them | exclude their evolutions
+        # melee
+        "amaterasu",  # 11/1
+        "freddy",  # 11/0
+        "ghosta",  # 11/0
+        "goliath",  # 11/0
+        "griffius",  # 11/1
+        "shao lin",  # 16/2
+        "sheer",  # 11/0
+
+        # ranged
+        "blair",  # 17/3
+        "gremory",  # 19/3
+        "melia",  # 11/2
+        "mizu",  # 11/0
+        "necro",  # 11/1
+        "yorik",  # 11/0
+        "yuri",  # 16/3
+    }
+    bobo_farming = {  # exclude these heroes | exclude their evolutions
+        # melee
+        "angelia",  # 1/2
+        "chuba",  # 1/3
+        "d'arc",  # 1/0
+        "dead lord",  # 1/2
+        "dominus",  # locked
+        "fury",  # 1/2
+        "legion",  # 1/2
+        "mi",  # 1/3
+        "scrap",  # 1/1
+
+        # ranged
+        "agony",  # 1/2
+        "aphro",  # 1/3
+        "frost",  # 1/1
+        "justia",  # 1/0
+        "tai ling",  # 1/1
+        "trix",  # 1/2
+        "vixen",  # locked
+
+        # building
+        "caesar",  # 1/2
+    }
+
+    file = Path("tests/HighGrowth/2022-02-03-0931_Bobo_squad_export.txt")
+    exclude = all_evolutions_to(bobo_squad) + all_evolutions_to(bobo_farming, inclusive=True) + all_evolutions_to(bobo_to_farm)
+    never_reborn = exactly({"charon"}) + exactly(bobo_to_farm)
+    gold_discount = Discount.combine(Discount.NIGHTMARE, Discount.VIP9)
+
+    return file, exclude, never_reborn, gold_discount
+
+
+def barf_settings():
+    file = None
+    exclude = None
+    never_reborn = None
+    gold_discount = Discount.combine(Discount.NIGHT_FALL, Discount.VIP13)
+
+    return file, exclude, never_reborn, gold_discount
+
+
+def seph_settings():
+    # never reborn any legendary heroes
+    # keep all of tai lings heroes at lvl 1, all of villano, ghosta, and justia at lvl 1.
+    # epics and rares of shaa, grace and eostre also at lvl 1 (some i accidently lvled).
+    seph_farming = {  # exclude these heroes | exclude their evolutions
+        "tai ling",
+        "villano",
+        "ghosta",
+        "justia",
+    }
+    seph_squad = {  # level/reborn these as much as possible | exclude their evolutions
+        "moona",
+        "grace",
+        "eostre"
+    }
+
+    file = Path("tests/HighGrowth/2022-01-31_Seph_squad_export.txt")
+    exclude = all_evolutions_to(seph_squad) + all_evolutions_to(seph_farming, inclusive=True)
+    never_reborn = has_rarity(Rarity.LEGENDARY)
+    gold_discount = Discount.combine(Discount.NIGHT_TERROR)
+
+    return file, exclude, never_reborn, gold_discount
+
+
+def souldead_settings():
+    file = None
+    exclude = None
+    never_reborn = None
+    gold_discount = Discount.combine(Discount.NIGHTMARE, Discount.VIP12)
+
+    return file, exclude, never_reborn, gold_discount
+
+
+# ======================================================================================================================
+# Runner
+# ----------------------------------------------------------------------------------------------------------------------
+
+file, exclude, never_reborn, gold_discount = seph_settings()
 
 logger.info(f"Squad file: {file}")
 
@@ -52,117 +168,11 @@ logger.info(f"Summary:\n{collection.summarize()}")
 
 logger.info("-" * 120)
 
-bobo_squad = {  # level/reborn these as much as possible | exclude their evolutions
-    # melee
-    "charon",
-    "groot",
-    "shaa",
-    "mosura",
-
-    # ranged
-    "blair",
-    "eostre",
-    "grace",
-    "strik",
-}
-bobo_to_farm = {  # level these heroes but never reborn them | exclude their evolutions
-    # melee
-    "diana",  # 21/3
-    "flap",  # 18/3
-    "freddy",  # 6/0
-    "ghosta",  # 6/0
-    "goliath",  # 8/0
-    "shao lin",  # 16/1
-    "sheer",  # 6/0
-
-    # ranged
-    "gremory",  # 19/3
-    "melia",  # 9/2
-    "mizu",  # 6/0
-    "necro",  # 6/1
-    "mina",  # 16/2
-    "yorik",  # 6/0
-    "yuri",  # 16/3
-}
-bobo_farming = {  # exclude these heroes | exclude their evolutions
-    # melee
-    "angelia",  # 1/2
-    "arthur",  # 1/1
-    "chuba",  # 1/3
-    "d'arc",  # 1/0
-    "dead lord",  # 1/2
-    "dominus",  # locked
-    "fury",  # 1/2
-    "legion",  # 1/2
-    "mi",  # 1/3
-    "scrap",  # 1/1
-
-    # ranged
-    "agony",  # 1/2
-    "aphro",  # 1/3
-    "frost",  # 1/1
-    "justia",  # 1/0
-    "trix",  # 1/2
-    "vixen",  # locked
-    "villano",  # 1/1
-
-    # building
-    "caesar",  # 1/1
-}
-
-not_adam_farming = {  # exclude these heroes + all evolutions to them
-    "vixen",
-    "scrap",
-    "mosura",
-    "aphro"
-}
-
-seph_farming = {
-    "alexandria",
-    "dominus"
-}
-
 strategy = MinimizeGold(
     collection=collection,
-
-    # BabyBobo:
-    # none
-
-    # Bobo:
-    exclude=all_evolutions_to(bobo_squad) + all_evolutions_to(bobo_farming, inclusive=True) + all_evolutions_to(bobo_to_farm),
-    never_reborn=exactly({"charon"}) + exactly(bobo_to_farm),
-
-    # Gravy:
-    # none
-
-    # JoeDaddy:
-    # none
-
-    # MikeLouie:
-    # none
-
-    # Minato:
-    # exclude=exactly({"draggara", "yorik"}),
-    # never_reborn=exactly({"grace", "charon"}),
-
-    # NotAdam:
-    # exclude=all_evolutions_to(not_adam_farming, inclusive=True),
-
-    # Seph:
-    # exclude=all_evolutions_to(seph_farming, inclusive=True),
-    # never_reborn=none(),
-
-    # SirBrychee:
-    # exclude=has_rarity(Rarity.LEGENDARY),
-    # never_reborn=none(),  # + has_rarity(Rarity.EPIC),
-
-    # SoulD3aD:
-    # none
-
-    # Stalguard:
-    # exclude=all_evolutions_to({"dominus", "mosura"}, inclusive=True) + all_evolutions_to({"grace"}),
-
-    gold_discount=Discount.combine(Discount.NIGHTMARE, Discount.VIP8),
+    exclude=exclude,
+    never_reborn=never_reborn,
+    gold_discount=gold_discount,
 )
 
 # calc = HighGrowthCalculation.with_gold_cap(
@@ -173,8 +183,8 @@ strategy = MinimizeGold(
 
 calc = HighGrowthCalculation.for_level_ups(
      strategy=strategy,
-     level_ups_already_completed=CompletionTier.aggregate_to(CompletionTier.TIER_7)[0],
-     level_ups_goal=CompletionTier.aggregate_to(CompletionTier.TIER_8)[0]
+     # level_ups_already_completed=CompletionTier.aggregate_to(CompletionTier.TIER_7)[0],
+     level_ups_goal=CompletionTier.aggregate_to(CompletionTier.TIER_10)[0]
 )
 
 logger.info(calc)
